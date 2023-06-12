3b0a6ef6d9accb000fe67a9a0a4596820a8a6c40
Jayme Anunciação <jayme.anunciacao@gmail.com>
2023-06-12 03:13:52 +0000

    feature(App): Estrutura a sidebar e o formulário de tarefas.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
-  //
+  <main class="columns is-gapless is-multline">
+    <div class="column is-one-quarter">
+      <SideBar />
+    </div>
+    <div class="column is-three-quarter">
+      <FormSection />
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+import SideBar from '@/components/SideBar.vue';
+import FormSection from '@/components/Form.vue';
+
 export default defineComponent({
   name: 'App',
+  components: {
+    SideBar,
+    FormSection,
+  }
 });
 </script>
