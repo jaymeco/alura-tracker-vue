ff26d7eeebb8b2b0aec9a4c4f6275ac41dbceca3
Jayme Anunciação <jayme.anunciacao@gmail.com>
2023-07-08 16:13:46 +0000

    feature(components/TaskItem): Cria componente de item de tarefa.

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="box has-text-weight-bold">
+    <div class="columns">
+      <div class="column is-7">
+        Descrição da tarefa
+      </div>
+      <div class="column">
+        <StopWatch :seconds="15" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import StopWatch from './StopWatch.vue';
+
+export default defineComponent({
+  name: 'TaskItem',
+  components: {
+    StopWatch,
+  },
+})
+</script>
+
+<style scoped>
+.box {
+  background-color: #FAF0CA;
+}
+</style>
