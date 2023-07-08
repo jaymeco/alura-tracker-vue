6457781a10ce1cff2f679582af763af89590561c
Jayme Anunciação <jayme.anunciacao@gmail.com>
2023-07-08 20:06:05 +0000

    feature(components/SideBar): Adiciona emissão de evento para a troca do tema da aplicação.

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -3,15 +3,32 @@
     <h1>
       <img src="../assets/logo.png" width="100" alt="logo">
     </h1>
+    <button class="button" @click="switchTheme">{{ buttonLabel }}</button>
   </header>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue';
+import { defineComponent } from 'vue';
 
-  export default defineComponent({
-    name: 'SideBar',
-  });
+export default defineComponent({
+  name: 'SideBar',
+  emits: ['onChangeTheme'],
+  data: () => ({
+    darkMode: false,
+  }),
+  computed: {
+    buttonLabel() {
+      return this.darkMode ? 'Desativar modo escuro'
+        : 'Ativar modo escuro';
+    },
+  },
+  methods: {
+    switchTheme() {
+      this.darkMode = !this.darkMode
+      this.$emit('onChangeTheme', this.darkMode)
+    }
+  }
+});
 </script>
 
 <style scoped>
@@ -21,8 +38,10 @@ header {
   width: 100%;
   height: 100vh;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
+  align-items: center;
 }
+
 @media only screen and (max-width: 768px) {
   header {
     padding: 2.5rem;
