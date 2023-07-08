e0cbf58db32a60ed2c5c6ac0f014a0e3595114b3
Jayme Anunciação <jayme.anunciacao@gmail.com>
2023-07-08 16:45:23 +0000

    feature(App): Implementa listagem e salvamento de tarefas.

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,10 @@
       <SideBar />
     </div>
     <div class="column is-three-quarter">
-      <FormSection />
+      <FormSection @on-submit="saveTask" />
+      <div class="taks-list">
+        <TaskItem v-for="(task, index) in tasks" v-bind:key="index" :task="task"/>
+      </div>
     </div>
   </main>
 </template>
@@ -12,14 +15,32 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+import Task from './interfaces/Task';
+
 import SideBar from '@/components/SideBar.vue';
 import FormSection from '@/components/Form.vue';
+import TaskItem from '@/components/TaskItem.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     SideBar,
     FormSection,
-  }
+    TaskItem
+  },
+  data: () => ({
+    tasks: [] as Task[],
+  }),
+  methods: {
+    saveTask(task: Task) {
+      this.tasks.push(task);
+    },
+  },
 });
 </script>
+
+<style scoped>
+.taks-list {
+  padding: 1.25em;
+}
+</style>
