e6b1f2cc708bbdf5e61805b67afac5376dfa7223
Jayme Anunciação <jayme.anunciacao@gmail.com>
2023-07-08 15:41:42 +0000

    feature(components/IconButton): Cria componente genérico de botão com icones.

---
+++ src/components/IconButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <button class="button" @click="triggerAction" :disabled="disabled">
+    <span class="icon">
+      <i :class="'fas ' + iconName"></i>
+    </span>
+    <span>{{ label }}</span>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'IconButton',
+  props: {
+    action: {
+      type: Function,
+      required: true,
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    iconName: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    triggerAction() {
+      this.action();
+    }
+  }
+});
+
+</script>
+
+<style scoped></style>
