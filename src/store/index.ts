68282668d9e63d4376344fdf2aa38d43576d59f5
Jayme Anunciação <jayme.anunciacao@gmail.com>
2023-09-03 02:27:06 +0000

    feature(store): Adiciona mutation para a adição de um project.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,6 @@
 import Project from "@/interfaces/Project";
 import { InjectionKey } from "vue";
-import { createStore, Store } from 'vuex';
+import { createStore, Store, useStore } from 'vuex';
 
 interface AppState {
   projects: Project[];
@@ -11,5 +11,19 @@ export const key: InjectionKey<Store<AppState>> = Symbol();
 export const store = createStore<AppState>({
   state: {
     projects: [],
+  },
+  mutations: {
+    'ADD_PROJECT'(state, name: string) {
+      const project: Project = {
+        id: (new Date()).toISOString(),
+        name,
+      };
+
+      state.projects.push(project);
+    },
   }
 });
+
+export function useAppStore(): Store<AppState> {
+  return useStore(key);
+}
