d932a4f38198c5867daa3407f0e92e44da904530
Jayme Anunciação <jayme.anunciacao@gmail.com>
2023-09-03 16:12:38 +0000

    feature(store): Adiciona mutation para adição de notificações.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
+import Notification, { NotificationType } from "@/interfaces/Notification";
 import Project from "@/interfaces/Project";
 import { InjectionKey } from "vue";
 import { createStore, Store, useStore } from 'vuex';
 
 interface AppState {
   projects: Project[];
+  notifications: Notification[];
 }
 
 export const key: InjectionKey<Store<AppState>> = Symbol();
@@ -11,6 +13,7 @@ export const key: InjectionKey<Store<AppState>> = Symbol();
 export const store = createStore<AppState>({
   state: {
     projects: [],
+    notifications: [],
   },
   mutations: {
     'ADD_PROJECT'(state, name: string) {
@@ -35,7 +38,18 @@ export const store = createStore<AppState>({
     },
     'DELETE_PROJECT'(state, projectId: string) {
       state.projects = state.projects.filter(({ id }) => id !== projectId);
-    }
+    },
+    'NOTIFICATE'(state, newNotification: Notification) {
+      newNotification = {
+        ...newNotification,
+        id: (new Date()).getTime(),
+      }
+      state.notifications.push(newNotification);
+
+      setTimeout(() => {
+        state.notifications = state.notifications.filter(({ id }) => id !== newNotification.id)
+      }, 2000);
+    },
   }
 });
 
