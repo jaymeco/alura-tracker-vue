705b60fbea1e819e8135a6a5a601b3b070edec35
Jayme Anunciação <jayme.anunciacao@gmail.com>
2023-09-03 01:20:45 +0000

    feature(router): Adiciona rota para a página de projetos;

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,17 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 import TaskPage from '../pages/TaskPage.vue';
-
+import ProjectsPage from '../pages/ProjectsPage.vue';
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'tasks',
     component: TaskPage,
+  },
+  {
+    path: '/projects',
+    name: 'Projects',
+    component: ProjectsPage,
   }
 ];
 
