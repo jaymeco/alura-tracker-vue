c63282c85ee98230988c09874fcdab19c123bbd7
Jayme Anunciação <jayme.anunciacao@gmail.com>
2023-09-03 01:06:37 +0000

    refactor(app): Utiliza router view para renderização da página.

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,7 @@
       <SideBar @on-change-theme="switchTheme" />
     </div>
     <div class="column is-three-quarter content">
-      <FormSection @on-submit="saveTask" />
-      <div class="taks-list">
-        <BoxContainer v-if="isEmptyTasks">
-          Você não possui nenhuma produção hoje :(
-        </BoxContainer>
-        <TaskItem v-for="(task, index) in taskList.items" v-bind:key="index" :task="task" v-bind:sequence="index + 1" />
-      </div>
+      <router-view></router-view>
     </div>
   </main>
 </template>
@@ -18,35 +12,17 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 
-import Task from '@/entities/Task';
-import List from '@/classes/List';
-
 import SideBar from '@/components/SideBar.vue';
-import FormSection from '@/components/Form.vue';
-import TaskItem from '@/components/TaskItem.vue';
-import BoxContainer from './components/BoxContainer.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
     SideBar,
-    FormSection,
-    TaskItem,
-    BoxContainer
   },
   data: () => ({
-    taskList: new List<Task>(),
     isDarkMode: false,
   }),
-  computed: {
-    isEmptyTasks() {
-      return this.taskList.isEmpty;
-    },
-  },
   methods: {
-    saveTask(task: Task) {
-      this.taskList.add(task);
-    },
     switchTheme(darkMode: boolean) {
       this.isDarkMode = darkMode;
     }
