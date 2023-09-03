59349f50820040c017a4431d071b8505b6112d1d
Jayme Anunciação <jayme.anunciacao@gmail.com>
2023-09-03 02:44:45 +0000

    feature(pages/CreateProjectPage): Cria página de cadastro de projeto.

---
+++ src/pages/CreateProjectPage.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="projetos">
+    <h1 class="title">Adicionar um projeto</h1>
+
+    <form @submit.prevent="handleSubmit">
+      <div class="field">
+        <label for="name" class="label">Nome do projeto</label>
+        <input type="text" class="input" v-model="project.name" id="project-name">
+      </div>
+
+      <div class="field">
+        <button type="submit" class="button">
+          Salvar
+        </button>
+      </div>
+    </form>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useAppStore } from '../store';
+
+export default defineComponent({
+  name: 'CreateProjectPage',
+  data() {
+    return {
+      project: {
+        name: ''
+      },
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.store.commit('ADD_PROJECT', this.project.name);
+      this.project.name = '';
+      this.$router.push('/projects');
+    }
+  },
+  setup() {
+    const store = useAppStore()
+
+    return {
+      store,
+    }
+  }
+});
+
+</script>
+
+<style scoped>
+.projetos {
+  padding: 1.25rem;
+}
+</style>
